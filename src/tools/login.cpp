feca5b0881fe92c58f9e25757c0422582c5815ad
MiChan <2417577316@qq.com>
2022-02-12 20:49:21 +0800

    Register Mode finish

--- src/tools/login.cpp
+++ src/tools/login.cpp
@@ -17,12 +17,12 @@ static const char* MODES = "\
 // this is the function where when entering 1 need userName and
 // password
 void Login_Enter(){
-
+    printf("%s%sLogin Successfully!%s",BOLD,FRONT_GREEN,RESET);
 }
 
 
 // Find whether the user exist in history;
-// if find return the password; else return -1;
+// if find return state+password; else return NOFIND;
 string FindUser(string user_name){
     string cont;
 
@@ -30,13 +30,34 @@ string FindUser(string user_name){
     string uspass;
     string uskind;
 
-    ifstream fin(FILEPATH);
+    ifstream fin(FILEPATH,ios_base::in);
     while(getline(fin,cont)){
         string temp;
         int N = cont.size();
-        cout<<"testign"<<cont<<" "<< N <<endl;
+        for(int i=0;i<N;i++){
+            // if ptr is not "space", add to temp to form the name
+            if(cont[i] != ' '){
+                temp = temp+cont[i];
+                continue;
+            }
+            // now temp should hold a name and prepare to compare with input
+            if(temp == user_name){
+                string rest;
+                for(;i<N;i++){
+                    if(cont[i]!=' '){
+                        rest = rest + cont[i];
+                    }
+                }
+                return rest;
+            }
+            // we should reset the temp
+            else{
+                temp = "";
+            }
+            
+        }
     }
-    return "X";
+    return "NOFIND";
 }
 
 
@@ -44,8 +65,9 @@ void LoginPage(){
     string mode_choose;
     string UserName;
     string password;
-    ofstream fout(FILEPATH);
-    ifstream fin(FILEPATH);
+    ofstream fout(FILEPATH,ios_base::app|ios_base::out);
+    ifstream fin(FILEPATH,ios_base::in);
+    
     
     printf("\n%s%sWelcome to%s %s%sShengjian Market%s\n",BOLD,FRONT_RED,RESET,BOLD,FRONT_BLUE,RESET);
     printf("%s%s%s%s\n", FRONT_BLUE,BOLD,MODES,RESET);
@@ -55,25 +77,40 @@ void LoginPage(){
         cin>>mode_choose;
 
         if(mode_choose == "1"){
-            // 
-            FindUser("X");
+            string SandP;
+
             Login_Enter();
         }
         else if(mode_choose == "2"){
             cout<<"Please Enter Your User Name: ";
             cin>>UserName;
-
-            cout<<"Please Enter Your Password: ";
+            string SandP;
+            SandP = FindUser(UserName);
+            while(SandP!="NOFIND"){
+                printf("%s%sName Was Taken!%s",BOLD,FRONT_RED,RESET);
+                printf("Please %s%sRE-%sEnter Your User Name: ",BOLD,FRONT_RED,RESET);
+                cin>>UserName;
+                SandP = FindUser(UserName);
+            }
+            
+            cout<<"Please Enter Your PassWord: ";
             cin>>password;
-            fout<<UserName<<" "<<password<<endl;
+            
+            fout<<UserName<<" Usr "<<password<<endl;
+            Login_Enter();
+            break;
         }
-        else if(mode_choose =="4"){
+        else if(mode_choose == "3"){
+            printf("\t\t\t   %s%sYou Are Entering Admin Mode%s\n",BOLD,FRONT_PURPLR,RESET);
+            break;
+        }
+        else if(mode_choose == "4"){
             break;
         }
         else{
             printf("%s%sERROR INPUT! PLEASE RE-ENTER%s\n",BOLD,FRONT_RED,RESET);
         }
     }
-    fout.close();
+    //fout.close();
     fin.close();
 }
